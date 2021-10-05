28373c132d5c8e9134ffde9eb21304c513c51da3
leonardoCatharin <leo_catharin@hotmail.com>
2021-10-05 15:56:10 -0300

    primeira pagina

--- src/pages/grid/grid.ts
+++ src/pages/grid/grid.ts
@@ -7,7 +7,43 @@ import { NavController } from 'ionic-angular';
     templateUrl: 'grid.html',
 })
 export class Grid {
+    public segment = '0';
+
+    public imagens = [
+        {
+            path    : 'src/assets/img/imagens1.jpg',
+            titulo  : 'Imagem 1'
+        },
+        {
+            path    : 'src/assets/img/imagens2.jpg',
+            titulo  : 'Imagem 2'
+        }
+    ]
+
+    public pessoa = {
+        nome : '',
+        sexo : 'M',
+        data : new Date(),
+        rg   : '',
+        cpf  : ''
+    }
+
+    public opcoes = [
+        { valor : 'M', descricao : 'Masculino'},
+        { valor : 'F', descricao : 'Feminino'}
+    ]
 
     constructor() {
     }
+
+    change(){
+        console.log('segment change');
+    }
+
+    refresh(refresher){
+        setTimeout(() => {
+            refresher.complete();
+        }, 2000);
+    }
+
 }
